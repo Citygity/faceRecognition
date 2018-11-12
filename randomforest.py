5d73e1715fa6299f601c48706e56ef1edeea7201
Citygity <316495987@qq.com>
2018-11-12 23:48:11 +0800

    pre-processing

--- randomforest.py
+++ randomforest.py
@@ -11,16 +11,13 @@ from sklearn.metrics import classification_report
 import pandas as pd
 import numpy as np
 
-iris = load_iris()   # 这里是sklearn中自带的一部分数据
-df = pd.DataFrame(iris.data, columns=iris.feature_names) # 格式化数据
+
 trainData=pd.read_csv('D:\\code\\pycode\\lipstick\\train_data.csv')
 trainDataLable=pd.read_csv('D:\\code\\pycode\\lipstick\\train_labels.csv')
 
 trainData=trainData.join(trainDataLable)
 
-print (df)          # 
-trainData['is_train'] = np.random.uniform(0, 1, len(trainData)) <= .75
-df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+trainData['is_train'] = np.random.uniform(0, 1, len(trainData)) <= .90
 
 train, test = trainData[trainData['is_train']==True], trainData[trainData['is_train']==False]
 
